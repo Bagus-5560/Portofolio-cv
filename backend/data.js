6a86b5cf9521ceb2753bbdb1986a37bc40397a6f
Bagus-5560 <bagusputrawiratama24@gmail.com>
2025-07-04 17:01:00 +0700

    Tahap Awal

---
+++ backend/data.js
@@ -0,0 +1,18 @@
+// backend/data.js
+const educationHistory = [
+{ id: 1, period: '2022 - Sekarang', institution: 'Universitas Gadjah Mada', major: 'S1 - Teknik Informatika' },
+{ id: 2, period: '2019 - 2022', institution: 'SMA Negeri 1 Teladan Yogyakarta', major: 'MIPA' }
+];
+const skills = [
+{ name: 'Vue.js', level: 'Mahir' }, { name: 'JavaScript', level:'Mahir' },
+{ name: 'Tailwind CSS', level: 'Mahir' }, { name: 'Node.js', level:'Menengah' },
+{ name: 'Express.js', level: 'Menengah' }, { name: 'PostgreSQL',level: 'Menengah' },
+{ name: 'Git & GitHub', level: 'Mahir' }, { name: 'HTML5 & CSS3',level: 'Mahir' },
+];
+const projects = [ 
+{ name: 'Vue.js', level: 'Mahir' }, { name: 'JavaScript', level:'Mahir' },
+{ name: 'Tailwind CSS', level: 'Mahir' }, { name: 'Node.js', level:'Menengah' },
+{ name: 'Express.js', level: 'Menengah' }, { name: 'PostgreSQL',level: 'Menengah' },
+{ name: 'Git & GitHub', level: 'Mahir' }, { name: 'HTML5 & CSS3',level: 'Mahir' },
+];
+module.exports = { educationHistory, skills, projects };
