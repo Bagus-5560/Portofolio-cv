6a0d9304edd07e14d439ebcf323448148fb85218
REXSY06 <bagusputrawiratama24@gmail.com>
2025-07-08 22:32:34 +0700

    Tambah Footer

--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,7 @@
 // backend/data.js
 const educationHistory = [
-{ id: 1, period: '2022 - Sekarang', institution: 'Universitas Gadjah Mada', major: 'S1 - Teknik Informatika' },
-{ id: 2, period: '2019 - 2022', institution: 'SMA Negeri 1 Teladan Yogyakarta', major: 'MIPA' }
+{ id: 1, period: '2019 - 2022', institution: 'SMK Negeri 2 Temanggung', major: 'Teknik Komputer dan Jaringan' },
+{ id: 2, period: '2022 - Sekarang', institution: 'Universitas AMIKOM', major: 'S1 - Teknik Informatika' }
 ];
 const skills = [
 { name: 'Vue.js', level: 'Mahir' }, { name: 'JavaScript', level:'Mahir' },
