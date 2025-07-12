408878deee868dc7de587e10d2e68fd35482200a
REXSY06 <bagusputrawiratama24@gmail.com>
2025-07-12 10:28:16 +0700

    remove test(failed test)

--- api/test.js
+++
@@ -1,6 +0,0 @@
-// api/test.js
-export default function handler(request, response) {
-  response.status(200).json({ 
-    message: 'Tes API berhasil! Jika kamu lihat ini, routing dasar Vercel bekerja.' 
-  });
-}
